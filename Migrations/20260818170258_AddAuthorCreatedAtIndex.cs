66a3d6820884e223e4a3be716fa60eb73d1ffbaf
clever <sanya.veselkov.811@gmail.com>
2026-08-18 20:53:04 +0300

    feat: added index on AuthorId and CreatedAt

---
+++ Migrations/20260818170258_AddAuthorCreatedAtIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace explAInedCommentService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuthorCreatedAtIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_AuthorId_CreatedAt",
+                table: "Comments",
+                columns: new[] { "AuthorId", "CreatedAt" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_AuthorId_CreatedAt",
+                table: "Comments");
+        }
+    }
+}
